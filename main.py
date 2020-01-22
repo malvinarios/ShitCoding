61b2ad9e3ce1fd78c9c4eeafe5ccec4c09a9d66b
Y4kuzi <help@provisionweb.org>
2020-01-22 06:04:02 +0100

    connect

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ class Bot:
         self.server = "irc.provisionweb.org"
         self.port = 6667
         self.nick = "ShitCodedBot"
+        self.ident = "shit"
         self.sock = socket.socket()
 
     def run(self):
         self.sock.connect((self.server, self.port))
         print('Connected')
+        self.reg_client()
+
+    def reg_client(self):
+        self.raw('NICK '+self.nick)
+        self.raw('USER {} 0 0 :Shit bot'.format(self.ident))
+
+    def raw(self, data):
+        self.sock.send(bytes(data, 'utf-8'))
+
+
+    def read_sock(self):
+        pass
+        # Here we will read the socket.
 
 b = Bot()
 b.run()
