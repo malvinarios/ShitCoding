2e439745ead4b3727a47b2524a094ebe66a3fa8d
Y4kuzi <help@provisionweb.org>
2020-01-22 05:49:11 +0100

    connect

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 print('Sup world!')
 
-for _ in range 10:
-  print(f"Go nuts {_} time(s)")
+import socket
+
+class Bot:
+    def __init__(self):
+        self.server = "irc.provisionweb.org"
+        self.port = 6667
+        self.nick = "ShitCodedBot"
+        self.sock = socket.socket()
+
+    def run(self):
+        self.sock.connect((self.server, self.port))
+        print('Connected')
+
+b = Bot()
+b.run()
